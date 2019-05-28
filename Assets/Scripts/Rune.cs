deed8980ebfdb4703c2323dc1345175405137d43
Matías Cymberknoh <elmatyzz777@hotmail.com>
2019-05-28 16:30:44 -0300

    Agregada Runa de Curación

---
+++ Assets/Scripts/Rune.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rune : MonoBehaviour
+{
+    public float healingAmount;
+    bool used;
+    Renderer r;
+
+    void Start()
+    {
+        used = false;
+        r = GetComponent<Renderer>();
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (!used)
+        {
+            Model player = c.gameObject.GetComponent<Model>();
+            if (player)
+            {
+                player.life += healingAmount;
+                player.view.UpdateLifeBar(player.life / player.maxLife);
+                StartCoroutine(Use());
+            }
+        }
+    }
+
+    IEnumerator Use()
+    {
+        used = true;
+        float time = 0;
+
+        while (time < 1)
+        {
+            time += Time.deltaTime;
+            float red = Mathf.Lerp(0, 255, time);
+            r.material.color = new Color(red, 0, 0);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
